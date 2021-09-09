012f306364439ffc69a2268d16bcb7c6b6e93fc7
Prashant Singh <60733103+rc21292@users.noreply.github.com>
2021-09-09 13:01:20 +0530

    Initial Commit

---
+++ coreui/src/views/Login.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import TheLogin from './pages/login/Login'
+import {
+	CHeader,
+	CHeaderBrand,
+	CHeaderNav,
+	CHeaderNavItem,
+	CHeaderNavLink
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+const Login = () => {
+	return (
+		<div className="c-app c-default-layout">
+
+		<div className="c-wrapper">
+		<CHeader withSubheader>
+
+		<CHeaderBrand className="mx-auto d-lg-none" to="/">
+		<CIcon name="logo" height="48" alt="Logo"/>
+		</CHeaderBrand>
+
+		<CHeaderNav className="d-md-down-none mr-auto">
+		<CHeaderNavItem className="px-5" >
+		<CHeaderNavLink to="/">Home</CHeaderNavLink>
+		</CHeaderNavItem>
+		</CHeaderNav>
+
+		<CHeaderNav className="px-3">
+		<CHeaderNavItem  className="px-3 c-active">
+		<CHeaderNavLink to="/login">Login</CHeaderNavLink>
+		</CHeaderNavItem>
+		<CHeaderNavItem className="px-3">
+		<CHeaderNavLink to="/register">Register</CHeaderNavLink>
+		</CHeaderNavItem>
+		</CHeaderNav>
+
+		</CHeader>
+
+		<div className="c-body">
+		<TheLogin/>
+		</div>
+		</div>
+		</div>
+		)
+	}
+
+	export default Login
---
+++ laravel/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required|string|max:191',
+            'email'     => 'required|email|max:191|unique:users,email',
+            'password'  =>'required|min:8|confirmed'
+        ]);
+
+        if($validator->fails())
+        {
+
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+
+        }else{
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken($user->email.'_token', ['server:update'])->plainTextToken;
+
+            return response()->json([
+                'status'=> 200,
+                'username' => $user->name,
+                'token' => $token,
+                'message'=>'Registered Successfully', 
+            ]);
+        }
+    }
+
+    public function login(Request $request){
+       $validator = Validator::make($request->all(), [
+        'email'     => 'required|email|max:191',
+        'password'  => 'required|min:8|'
+    ]);
+
+       if($validator->fails())
+       {
+
+        return response()->json([
+            'validation_errors' => $validator->messages(),
+        ]);
+
+    }else{
+
+       $user = User::where('email', $request->email)->first();
+
+       if (! $user || ! Hash::check($request->password, $user->password)) {
+
+        return response()->json([
+            'status' => 401,
+            'message' => 'Invalid Credentials',
+        ]);
+
+    }else{
+       $token = $user->createToken($user->email.'_token', ['server:update'])->plainTextToken;
+
+       return response()->json([
+        'status'=> 200,
+        'username' => $user->name,
+        'token' => $token,
+        'message'=>'Welcome '.$user->name , 
+    ]);
+   }
+
+}
+}
+}
